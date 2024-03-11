3b2542929b46f8c4867fc4ae61a0b0d7309e14bb
김유현 <kinyuhyun@gim-yuhyeons-MacBook-Air.local>
2024-03-11 20:27:00 +0900

    add logics and test file c

--- packages/chapter1/src/a.js
+++ packages/chapter1/src/a.js
@@ -23,7 +23,8 @@ function Worker(health) {
 }
 
 function JuniorEngineer(health, intelligence) {
-  this._super(health);
+  // this._super(health);
+  Worker.call(this, health);
   this._intelligence = intelligence ?? 1;
   if (this._intelligence > 10) {
     this._isBornGenius = true;
@@ -34,7 +35,25 @@ function JuniorEngineer(health, intelligence) {
 // 여기에 코드를 작성하세요
 // TO-DO
 //- 여기에 코드를 작성하세요
+Worker.prototype.getHealth = function () {
+  return this._health;
+};
 
+Worker.prototype.work = function () {
+  this._health--;
+};
+
+JuniorEngineer.prototype = Object.create(Worker.prototype);
+JuniorEngineer.prototype.constructor = JuniorEngineer;
+
+JuniorEngineer.prototype.getIntelligence = function () {
+  return this._intelligence;
+};
+
+JuniorEngineer.prototype.work = function () {
+  Worker.prototype.work.call(this);
+  this._intelligence++;
+};
 /**
  * ## 문제 A - 추가문제
  *
